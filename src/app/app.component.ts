15463390c0bee9b661bb9fe48489942c0128bf3c
Bullettime23 <letsgofrontend@gmail.com>
2023-07-02 10:07:10 +0600

    feat: Add Articles to display content
    feat:
    - add ArticleComponent to manipulate the view
    - add ArticleModel for article instance logics incapsulation
    
    remove:
    - old hello-world components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'my-demo-app';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    console.log('Add ', title.value, ' and link ', link.value);
+  }
 }
